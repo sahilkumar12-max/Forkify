dc8b65fb97cf240eb8f9a03fe23f4350382af908
sahilkumar12-max <68015118+sahilkumar12-max@users.noreply.github.com>
2023-10-02 23:34:48 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3841c3ac-526e-43b0-a29d-813c322eaff3';
+export const MODAL_CLOSE_SEC = 2.5
